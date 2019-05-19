94ccb702d51084d0fb02803d46a9f3a6a125e51b
abba23 <abba23@users.noreply.github.com>
2019-05-19 12:24:21 +0200

    Update whitelist

--- whitelist.h
+++ whitelist.h
@@ -1,13 +1,15 @@
-static const char *whitelist[9] = {
+static const char *whitelist[12] = {
     "ap.spotify.com", // audio (access point)
+    "*.ap.spotify.com", // resolved access points
+    "apresolve.spotify.com", // access point resolving
+    "charts-images.scdn.co", // charts images
+    "dailymix-images.scdn.co", // daily mix images
     "i.scdn.co", // cover art
+    "lineup-images.scdn.co", // playlists lineup images
+    "merch-img.scdn.co", // merch images
+    "mosaic.scdn.co", // playlist mosaic images
     "pl.scdn.co", // playlist images
     "t.scdn.co", // background images
-    "charts-images.scdn.co", // charts images
-    "merch-img.scdn.co", // merch images
-    "lineup-images.scdn.co", // playlists lineup images
-    "mosaic.scdn.co", // playlist mosaic images
-    "thisis-images.scdn.co", // 'this is' playlists images
-    //"daily-mix.scdn.co",
+    "thisis-images.scdn.co" // 'this is' playlists images
     //"spclient.wg.spotify.com" // ads, concerts, radio, recently played, friend activity,...
 };
