79a823adbb3a573790c0bdd035e7070945185fdd
abba23 <abba23@users.noreply.github.com>
2019-12-14 21:01:18 +0100

    Update and clean up whitelist

--- whitelist.h
+++ whitelist.h
@@ -1,28 +1,31 @@
-static const char *whitelist[29] = {
+static const char *whitelist[32] = {
     "ap.spotify.com", // audio (access point)
     "*.ap.spotify.com", // resolved access points
     "apresolve.spotify.com", // access point resolving
     "audio-fa.scdn.co", // audio
     "audio-sp-*.pscdn.co", // audio
-    "audio-sp-*.spotifycdn.net", //audio
+    "audio-sp-*.spotifycdn.net", // audio
+    "audio4-ak.spotify.com.edgesuite.net", // audio
+    "heads4-ak-spotify-com.akamaized.net", // audio (heads)
     "audio4-fa.scdn.co", // audio
-    "audio.simplecast.com", // podcast
+    "audio.simplecast.com", // podcasts
     "charts-images.scdn.co", // charts images
-    "content.production.cdn.art19.com", // podcast
+    "content.production.cdn.art19.com", // podcasts
     "daily-mix.scdn.co", // daily mix images
     "dailymix-images.scdn.co", // daily mix images
     "dealer.spotify.com", // discord rich presence
+    "heads-fa.scdn.co", // audio (heads)
     "i.scdn.co", // cover art
     "image-upload.spotify.com", // image uploading
     "lineup-images.scdn.co", // playlists lineup images
-    "media.simplecast.com", // podcast
+    "media.simplecast.com", // podcasts
     "merch-img.scdn.co", // merch images
     "mosaic.scdn.co", // playlist mosaic images
     "newjams-images.scdn.co", // release radar images
     "o.scdn.co", // cover art
     "pl.scdn.co", // playlist images
     "profile-images.scdn.co", // artist profile images
-    "rss.art19.com", // podcast
+    "rss.art19.com", // podcasts
     "seeded-session-images.scdn.co", // radio images
     "spclient.wg.spotify.com", // ads/tracking (blocked in blacklist), radio, recently played, friend activity,...
     "t.scdn.co", // background images
