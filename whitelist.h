afa817aa9ca42422047bec87d1a60b027d9c7d39
abba23 <abba23@users.noreply.github.com>
2020-02-12 17:10:09 +0100

    Reorder whitelist

--- whitelist.h
+++ whitelist.h
@@ -1,36 +1,36 @@
 static const char *whitelist[] = {
-    "traffic.libsyn.com", // podcasts
-    "hwcdn.libsyn.com", // podcasts
-    "ap.spotify.com", // audio (access point)
-    "*.ap.spotify.com", // resolved access points
-    "apresolve.spotify.com", // access point resolving
-    "audio-fa.scdn.co", // audio
     "audio-sp-*.pscdn.co", // audio
-    "audio-sp-*.spotifycdn.net", // audio
-    "audio4-ak.spotify.com.edgesuite.net", // audio
-    "heads4-ak-spotify-com.akamaized.net", // audio (heads)
+    "audio-fa.scdn.co", // audio
     "audio4-fa.scdn.co", // audio
-    "audio.simplecast.com", // podcasts
     "charts-images.scdn.co", // charts images
-    "content.production.cdn.art19.com", // podcasts
     "daily-mix.scdn.co", // daily mix images
     "dailymix-images.scdn.co", // daily mix images
-    "dealer.spotify.com", // discord rich presence
     "heads-fa.scdn.co", // audio (heads)
     "i.scdn.co", // cover art
-    "image-upload.spotify.com", // image uploading
     "lineup-images.scdn.co", // playlists lineup images
-    "media.simplecast.com", // podcasts
     "merch-img.scdn.co", // merch images
     "mosaic.scdn.co", // playlist mosaic images
     "newjams-images.scdn.co", // release radar images
     "o.scdn.co", // cover art
     "pl.scdn.co", // playlist images
     "profile-images.scdn.co", // artist profile images
-    "rss.art19.com", // podcasts
     "seeded-session-images.scdn.co", // radio images
-    "spclient.wg.spotify.com", // ads/tracking (blocked in blacklist), radio, recently played, friend activity,...
     "t.scdn.co", // background images
     "thisis-images.scdn.co", // 'this is' playlists images
     "video-fa.scdn.co", // videos
+    "content.production.cdn.art19.com", // podcasts
+    "rss.art19.com", // podcasts
+    "hwcdn.libsyn.com", // podcasts
+    "traffic.libsyn.com", // podcasts
+    "audio.simplecast.com", // podcasts
+    "media.simplecast.com", // podcasts
+    "ap.spotify.com", // audio (access point)
+    "*.ap.spotify.com", // resolved access points
+    "apresolve.spotify.com", // access point resolving
+    "dealer.spotify.com", // discord rich presence
+    "image-upload.spotify.com", // image uploading
+    "spclient.wg.spotify.com", // ads/tracking (blocked in blacklist), radio, recently played, friend activity,...
+    "heads4-ak-spotify-com.akamaized.net", // audio (heads)
+    "audio4-ak.spotify.com.edgesuite.net", // audio
+    "audio-sp-*.spotifycdn.net", // audio
 };
