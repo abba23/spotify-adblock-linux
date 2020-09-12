d3954241cd4278902875ed447062cd087e0b4a1e
alsoGAMER <35269770+alsoGAMER@users.noreply.github.com>
2020-09-12 23:59:29 +0200

    Changes requested by @abba23
    - Moved *.buzzsprout.com under rss.art19.com
    - Changed the comment into the code, now there is only "podcasts"

--- whitelist.h
+++ whitelist.h
@@ -21,13 +21,13 @@ static const char *whitelist[] = {
     "video-fa.scdn.co", // videos
     "content.production.cdn.art19.com", // podcasts
     "rss.art19.com", // podcasts
+    "*.buzzsprout.com", // podcasts
     "platform-lookaside.fbsbx.com", // Facebook profile images
     "hwcdn.libsyn.com", // podcasts
     "traffic.libsyn.com", // podcasts
     "www.podtrac.com", // podcasts
     "audio.simplecast.com", // podcasts
     "media.simplecast.com", // podcasts
-    "*.buzzsprout.com", // podcasts e.g. new scientist
     "ap.spotify.com", // audio (access point)
     "*.ap.spotify.com", // resolved access points
     "apresolve.spotify.com", // access point resolving
