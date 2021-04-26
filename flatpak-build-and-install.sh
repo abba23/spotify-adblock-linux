0075729fed8becbe69362bb7eab5f5513785af28
rany2 <rany2@riseup.net>
2021-04-26 11:30:24 +0300

    Rename files to prevent confusion

--- flatpak-build-and-install.sh
+++ flatpak-build-and-install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+wget -O cef.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_88.1.6%2Bg4fe33a1%2Bchromium-88.0.4324.96_linux64_minimal.tar.bz2
+tar -xf cef.tar.bz2 --wildcards '*/include' --strip-components=1
 flatpak install -y flathub org.freedesktop.Sdk//20.08
 flatpak install -y flathub com.spotify.Client
 flatpak run --filesystem="$(pwd):rw" --command=bash org.freedesktop.Sdk//20.08 <<EOF
